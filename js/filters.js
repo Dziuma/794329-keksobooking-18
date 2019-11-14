1645b528ea710c8192b4b8965bf9829c9e17e286
Sancho <todom@ukr.net>
2019-11-14 22:46:50 +0200

    Добавляет фильтрацию по остальным фильтрам

--- js/filters.js
+++ js/filters.js
@@ -2,29 +2,67 @@
 
 (function () {
   var PINS_TO_SHOW = window.map.PINS_TO_SHOW;
+  var LOW_PRICE = 10000;
+  var HIGH_PRICE = 50000;
   var filterType = document.querySelector('#housing-type');
+  var filterPrice = document.querySelector('#housing-price');
+  var filterRooms = document.querySelector('#housing-rooms');
+  var filterGuests = document.querySelector('#housing-guests');
+  var filters = document.querySelectorAll('.map__filter, .map__checkbox');
 
   var matchesType = function (pinData, type) {
     return type === 'any' || pinData.offer.type === type;
+  };
+
+  var matchesPrice = function (pinData, price) {
+    return price === 'any' ||
+    (price === 'low' && pinData.offer.price < LOW_PRICE) ||
+    (price === 'middle' && pinData.offer.price >= LOW_PRICE && pinData.offer.price < HIGH_PRICE) ||
+    (price === 'high' && pinData.offer.price >= HIGH_PRICE);
+  };
+
+  var matchesRooms = function (pinData, rooms) {
+    return rooms === 'any' || pinData.offer.rooms === Number(rooms);
+  };
+
+  var matchesGuests = function (pinData, guests) {
+    return guests === 'any' || pinData.offer.guests === Number(guests);
+  };
+
+  var matchesFeatures = function (pinData) {
+    var features = Array.from(document.querySelectorAll('input[type=checkbox]:checked'));
+    return features.every(function (element) {
+      return pinData.offer.features.includes(element.value);
+    });
   };
 
   var filter = function (pinsData, config) {
     var filteredData = [];
 
     for (var i = 0; i < pinsData.length; i += 1) {
-      if (matchesType(pinsData[i], config)) {
+      if (matchesType(pinsData[i], config.type) &&
+      matchesPrice(pinsData[i], config.price) &&
+      matchesRooms(pinsData[i], config.rooms) &&
+      matchesGuests(pinsData[i], config.guests) &&
+      matchesFeatures(pinsData[i])) {
         filteredData.push(pinsData[i]);
-      }
-      if (filteredData.length === PINS_TO_SHOW) {
-        return filteredData;
+        if (filteredData.length === PINS_TO_SHOW) {
+          return filteredData;
+        }
       }
     }
 
     return filteredData;
   };
 
-  filterType.addEventListener('change', function () {
-    var pinsData = filter(window.map.loadedPinsData, filterType.value);
+  var filterChangeHandler = function () {
+    var config = {
+      type: filterType.value,
+      price: filterPrice.value,
+      rooms: filterRooms.value,
+      guests: filterGuests.value
+    };
+    var pinsData = filter(window.map.loadedPinsData, config);
     var pins = [];
 
     pinsData.forEach(function (pinData) {
@@ -35,5 +73,9 @@
 
     window.map.removePins();
     window.map.renderPins(pins);
+  };
+
+  filters.forEach(function (item) {
+    item.addEventListener('change', filterChangeHandler);
   });
 })();
